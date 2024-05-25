3b7b0427d48dea52fbadb8b030b8baa4a3a4026b
Mark Gavrilman <markgav19@gmail.com>
2024-05-25 07:59:01 +0300

    update

--- rollup-config/fileUtils.js
+++ rollup-config/fileUtils.js
@@ -1,33 +1,25 @@
 import sass from 'sass';
-
 import path from 'path';
 
 export function buildFileExtension(id){
 	const filename = path.parse(id).base;	
 	return filename.substring(filename.lastIndexOf('.')+1, filename.length) || filename;
-
 }
 
- export async function run_postcss( file)
-{
-	
-	 const result = await new Promise((resolve, reject) => {
-          sass.render({
-            file: file,
-            sourceMap: true,
-          }, (err, result) => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve(result)
-            }
-          })
-        })
-		
-		return result;
-	
-	
-	
+export async function run_postcss( file){	
+	const result = await new Promise((resolve, reject) => {
+		sass.render({
+				file: file,
+				sourceMap: true,
+		}, (err, result) => {
+			if (err) {
+				reject(err)
+			} else {
+				resolve(result)
+			}
+		})
+	})		
+	return result;
 }
 
 
